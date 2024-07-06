8b1627d6ca5f16323c5f23166d6abb4cd4c82fb9
Mochamad Rizky Ramadhan <mrizkyr2411@gmail.com>
2024-07-06 18:48:32 +0700

    change target url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
    server: {
       proxy: {
          '/api': {
-            target: 'http://localhost:3000',
+            target: 'https://tourist-attraction-api.vercel.app',
             changeOrigin: true,
          },
       },
